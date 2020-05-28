0df0d9da7eddc518c758cc1cf964602ae1ef67bc
Danex2 <khadane.miller@gmail.com>
2020-05-28 19:31:04 -0400

    initial commit

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
+  const [count, setCount] = React.useState(0);
+  const [timer, setTimer] = React.useState(60);
+  React.useEffect(() => {
+    const clickTimer = setInterval(() => {
+      if (timer > 0) {
+        setTimer((prevTimer) => prevTimer - 1);
+      }
+    }, 1000);
+    return () => clearInterval(clickTimer);
+  }, [timer]);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ flex: 1, marginTop: 50, fontSize: 30 }}>Clickr</Text>
+      <Text style={{ flex: 1, fontSize: 50 }}>{count}</Text>
+      <View style={{ flex: 2 }}>
+        <Button
+          onPress={() => setCount((prevCount) => prevCount + 1)}
+          title="Tap to update count!"
+          style={styles.button}
+          disabled={timer === 0}
+        />
+      </View>
+      <View style={{ flex: 1 }}>
+        <Text style={{ fontSize: 50 }}>{timer}</Text>
+      </View>
     </View>
   );
 }
@@ -12,8 +34,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+  },
+  button: {
+    height: 50,
+    width: 300,
+    flex: 1,
   },
 });
---
+++ README.md
@@ -0,0 +1,1 @@
+A stupid React Native app I made out of boredom, my high score is 318 try beating it
